5acf17ea8edc2bcacf3bf9d8c38516c7ec99b0c5
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-03-24 23:59:13 +0100

    feat: repast human-zombie moap simulation

---
+++ jzombies/src/jzombies/Human.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package jzombies;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import repast.simphony.engine.watcher.Watch;
+import repast.simphony.engine.watcher.WatcherTriggerSchedule;
+import repast.simphony.query.space.grid.GridCell;
+import repast.simphony.query.space.grid.GridCellNgh;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.SpatialMath;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+import repast.simphony.util.SimUtilities;
+
+/**
+ * @author anjolaoluwaadeuyi
+ *
+ */
+public class Human {
+
+	private ContinuousSpace<Object> space;
+	private Grid<Object> grid;
+	private int energy, startingEnergy;
+
+	private static final Logger logger = Logger.getLogger(Human.class.getName());
+
+	public Human(ContinuousSpace<Object> space, Grid<Object> grid, int energy) {
+		this.space = space;
+		this.grid = grid;
+		this.energy = startingEnergy = energy;
+
+		logger.info("I AM HERE XXXXXXXXXX");
+	}
+
+	@Watch(watcheeClassName = " jzombies.Zombie ", watcheeFieldNames = "moved", query = "within_moore 1", whenToTrigger = WatcherTriggerSchedule.IMMEDIATE)
+	public void run() {
+		GridPoint pt = grid.getLocation(this);
+
+		GridCellNgh<Zombie> nghCreator = new GridCellNgh<Zombie>(grid, pt, Zombie.class, 1, 1);
+		List<GridCell<Zombie>> gridCells = nghCreator.getNeighborhood(true);
+		SimUtilities.shuffle(gridCells, RandomHelper.getUniform());
+
+		GridPoint pointWithLeastZombies = null;
+		int minCount = Integer.MAX_VALUE;
+		for (GridCell<Zombie> cell : gridCells) {
+			if (cell.size() < minCount) {
+				pointWithLeastZombies = cell.getPoint();
+				minCount = cell.size();
+			}
+		}
+
+		if (energy > 0) {
+			moveTowards(pointWithLeastZombies);
+		} else {
+			energy = startingEnergy;
+		}
+	}
+
+	public void moveTowards(GridPoint pt) {
+		if (!pt.equals(grid.getLocation(this))) {
+			NdPoint myPoint = space.getLocation(this);
+			NdPoint otherPoint = new NdPoint(pt.getX(), pt.getY());
+			double angle = SpatialMath.calcAngleFor2DMovement(space, myPoint, otherPoint);
+			space.moveByVector(this, 2, angle, 0);
+			myPoint = space.getLocation(this);
+			grid.moveTo(this, (int) myPoint.getX(), (int) myPoint.getY());
+			energy--;
+		}
+	}
+
+}
